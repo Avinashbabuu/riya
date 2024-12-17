e73bad7957c97c94468742191d9c3f4696e7dc62
Avinashbabuu <158088631+Avinashbabuu@users.noreply.github.com>
2024-12-17 10:30:23 +0530

    config.py

--- config/config.py
+++ config/config.py
@@ -59,8 +59,8 @@ PRIVACY_LINK = getenv(
 )
 
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/C0DE_SEARCH")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AsuraaSupports")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Nepsxbot")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/THExNIGHTxCLUBbb")
 
 #❖ Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
